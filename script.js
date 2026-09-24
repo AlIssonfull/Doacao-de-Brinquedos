da341a387d6d01f8502c069577a3d7a9a028a9ac
Alisson <alisson.araujo.valorant@gmail.com>
2026-09-24 15:04:54 -0300

    Pagina Completa! Ajusta o menu de navegação para exibir links corretamente e melhora a responsividade

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-let btn = document.getElementById("btn");
-let menu = document.getElementById("menu");
-let menuC = document.getElementById("menuC");
-let nav = document.getElementById("nav")
-let nav2 = document.getElementById("nav2")
+const btn = document.getElementById("btn");
+const menu = document.getElementById("menu");
+const menuC = document.getElementById("menuC");
+const header = document.querySelector("header");
 
 menu.addEventListener("click", () => {
-    menuC.classList.add("menu");
-    menuC.classList.remove("mn");
-    menu.classList.add("mn");
-    menu.classList.remove("menu");
-    nav.style.display = "block"
-    nav2.style.display = "block"
-    nav.classList.add("navB")
-    nav2.style.width = "100%"
-    nav2.style.display = "flex"
-})
+    header.classList.add("menu-open");
+});
 
 menuC.addEventListener("click", () => {
-    menuC.classList.remove("menu");
-    menuC.classList.add("mn");
-    menu.classList.add("menu");
-    menu.classList.remove("mn");
-    nav.style.display = "none"
-    nav2.style.display = "none"
-})
+    header.classList.remove("menu-open");
+});
