93d82270b4c7aa0d3a1a80303739d459212b05fb
Alisson <alisson.araujo.valorant@gmail.com>
2026-09-24 17:00:34 -0300

    Completo! Adiciona funcionalidade de rolagem ao carrossel e ajusta layout das cartas

--- Compreoudoe.js
+++ Compreoudoe.js
@@ -10,3 +10,29 @@ menu.addEventListener("click", () => {
 menuC.addEventListener("click", () => {
     header.classList.remove("menu-open");
 });
+
+const carrossel = document.querySelector(".Carrossel");
+const setas = carrossel.querySelectorAll(".seta");
+let cards = [...carrossel.querySelectorAll(".card")];
+
+function atualizar() {
+    cards.forEach((card, i) => {
+        card.classList.remove("lef", "rig");
+        if (i === 0) {
+            card.classList.add("lef");
+        }
+        if (i === 2) {
+            card.classList.add("rig");
+        }
+    });
+}
+setas[0].onclick = () => {
+    cards.push(cards.shift());
+    atualizar();
+};
+setas[1].onclick = () => {
+    cards.unshift(cards.pop());
+    atualizar();
+};
+
+atualizar();
